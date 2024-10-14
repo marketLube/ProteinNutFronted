45ab7141fd935402c00e18a7864b7f4a5264b2c4
marketLube <marketlubes@gmail.com>
2024-10-14 21:48:00 +0530

    sl

--- src/Component/HomeLandingSection1.jsx
+++ src/Component/HomeLandingSection1.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import styles from './HomeLandingSection1.module.css';
+import React from "react";
+import styles from "./HomeLandingSection1.module.css";
 import { HiArrowCircleRight } from "react-icons/hi";
 
 export const HomeLandingSection1 = () => {
   return (
     <div className={styles.healthyflavr}>
       <h1 className={styles.head}>Healthy flavours. Infinite possibilities</h1>
-      <p className={styles.note}>Packed with Protein, Powered by Peanut Butter.</p>
+      <p className={styles.note}>
+        Packed with Protein, Powered by Peanut Butter.
+      </p>
       <button className={styles.shopnow}>
         <h2>Shop Now</h2>
-        <h1><HiArrowCircleRight /></h1>
+        <h1>
+          <HiArrowCircleRight />
+        </h1>
       </button>
     </div>
   );
-}
+};
