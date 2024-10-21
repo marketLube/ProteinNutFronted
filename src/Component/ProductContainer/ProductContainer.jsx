17eddbe3b98092362f0fac2cb01015e94bb27156
marketLube <marketlubes@gmail.com>
2024-10-21 12:09:59 +0530

    sl

--- src/Component/ProductContainer/ProductContainer.jsx
+++ src/Component/ProductContainer/ProductContainer.jsx
@@ -5,11 +5,6 @@ import { Link } from "react-router-dom";
 import { FaStar, FaStarHalf, FaStarHalfAlt } from "react-icons/fa";
 
 export const ProductContainer = ({ item }) => {
-  console.log(item)
-
-
-  console.log("product container");
-  
   return (
     <div
       className={styles.productcard}
