3d530c0dd8869c52ca2cd2f1eec8499b05af18ac
albertfredy7 <albertfredy007@gmail.com>
2024-10-23 11:33:01 +0530

    specific product details

--- src/Component/ProductContainer/ProductContainer.jsx
+++ src/Component/ProductContainer/ProductContainer.jsx
@@ -6,7 +6,7 @@ import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 export const ProductContainer = ({ product }) => {
   console.log(product);
 
-  const currentPrice = product.price - (product.offer / 100);
+  const currentPrice = product.price - (product.price * (product.offer / 100));
   console.log(currentPrice);
 
   console.log(product.avgRatings);
