24b9a1aa77f8a816bc6f89b48e60c2d65bf82ee7
Sanigamk <sanigamk@gmail.com>
2024-10-24 13:39:59 +0530

    responsive of cart

--- src/Component/ProductDisplay/ProductDetails.jsx
+++ src/Component/ProductDisplay/ProductDetails.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import styles from './ProductDetails.module.css'
-import { Counter } from '../Counter/Counter'
+import React from "react";
+import styles from "./ProductDetails.module.css";
+import { Counter } from "../Counter/Counter";
+import { useSelector } from "react-redux";
 
 export const ProductDetails = () => {
+  const isMobile = useSelector(function (state) {
+    return state.endpoint.isMobile;
+  });
+  console.log(isMobile, "iii");
+
   return (
     <div className={styles.card}>
       <div className={styles.header}>
@@ -11,21 +17,29 @@ export const ProductDetails = () => {
         <span>Total</span>
       </div>
       <div className={styles.productContainer}>
-        <img 
-          src="/Image/Untitled-8-01.png"
-          alt="Pineapple Peanut Butter"
-          className={styles.productImage}
-        />
-        <div className={styles.productInfo}>
-          <h3 className={styles.productTitle}>Pineapple flavoured </h3>
-          <h3 className={styles.productTitle}>Peanut Butter</h3>
-          <p className={styles.productPrice}>Rs. 349.00</p>
+        <div className={styles.imageContainer}>
+          <img
+            src="/Image/672px X 707px-01.svg"
+            alt="Pineapple Peanut Butter"
+            className={styles.productImage}
+          />
         </div>
-        <div className={styles.quantityContainer}>
-          <Counter/>
+        <div className={styles.det}>
+          <div className={styles.productInfo}>
+            <h3 className={styles.productTitle}>
+              Pineapple flavoured Peanut Butter
+            </h3>
+            {/* <h3 className={styles.productTitle}></h3> */}
+            <p className={styles.productPrice}>Rs. 349.00</p>
+          </div>
+          <div className={styles.quantityContainer}>
+            <Counter />
+          </div>
         </div>
-        <span className={styles.totalPrice}>Rs. 349.00</span>
+        {isMobile ? null : (
+          <span className={styles.totalPrice}>Rs. 349.00</span>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
